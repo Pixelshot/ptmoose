29bf04c9a0224d71de7a639b349d9307602fe970
Hazri Jordan <hazrijordan@gmail.com>
2018-08-11 21:24:59 +0800

    Installed latest material-ui/core and completed name and title for Header

---
+++ src/components/header_footer/Header.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/core/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+
+class Header extends Component {
+  render() {
+    return (
+      <AppBar
+        position="fixed"
+        style={{
+          backgroundColor: "#2f2f2f",
+          boxShadow: "none",
+          padding: "10px 0px"
+        }}
+      >
+        <Toolbar>
+          <div className="header_logo">
+            <div className="font_righteous header_logo_name">Moose Hormat</div>
+            <div className="header_logo_title">Personal Trainer</div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Header;

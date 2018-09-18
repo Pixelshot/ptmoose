ec28749e8cf1b40c5f2305a6f07fa42865ee83da
Hazri Jordan <hazrijordan@gmail.com>
2018-09-18 23:25:30 +0800

    Completed Nav and Side Drawer

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -3,14 +3,49 @@ import AppBar from "@material-ui/core/AppBar";
 import IconButton from "@material-ui/core/IconButton";
 import Gavel from "@material-ui/icons/Gavel";
 import Toolbar from "@material-ui/core/Toolbar";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core";
+import amber from "@material-ui/core/colors/amber";
 
+import SideDrawer from "./SideDrawer";
+const theme = createMuiTheme({
+  palette: {
+    primary: amber
+  }
+});
 class Header extends Component {
+  state = {
+    drawerOpen: false,
+    headerShow: false
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY > 0) {
+      this.setState({
+        headerShow: true
+      });
+    } else {
+      this.setState({
+        headerShow: false
+      });
+    }
+  };
+
+  toggleDrawer = value => {
+    this.setState({
+      drawerOpen: value
+    });
+  };
+
   render() {
     return (
       <AppBar
         position="fixed"
         style={{
-          backgroundColor: "#2f2f2f",
+          backgroundColor: this.state.headerShow ? "#2f2f2f" : "transparent",
           boxShadow: "none",
           padding: "10px 0px"
         }}
@@ -20,14 +55,20 @@ class Header extends Component {
             <div className="font_righteous header_logo_name">Moose Hormat</div>
             <div className="header_logo_title">Personal Trainer</div>
           </div>
+          <MuiThemeProvider theme={theme}>
+            <IconButton
+              aria-label="Menu"
+              color="inherit"
+              onClick={() => this.toggleDrawer(true)}
+            >
+              <Gavel color="primary" />
+            </IconButton>
+          </MuiThemeProvider>
 
-          <IconButton
-            aria-label="Menu"
-            color="inherit"
-            onClick={() => console.log("open")}
-          >
-            <Gavel />
-          </IconButton>
+          <SideDrawer
+            open={this.state.drawerOpen}
+            onClose={value => this.toggleDrawer(value)}
+          />
         </Toolbar>
       </AppBar>
     );
---
+++ src/components/header_footer/SideDrawer.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+
+const SideDrawer = props => {
+  return (
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClose={() => props.onClose(false)}
+    >
+      <List component="nav">
+        <ListItem button onClick={() => console.log("About")}>
+          About
+        </ListItem>
+        <ListItem button onClick={() => console.log("Services")}>
+          Services
+        </ListItem>
+        <ListItem button onClick={() => console.log("Testimonial")}>
+          Testimonial
+        </ListItem>
+        <ListItem button onClick={() => console.log("Location")}>
+          Location
+        </ListItem>
+        <ListItem button onClick={() => console.log("Contact")}>
+          Contact
+        </ListItem>
+        <ListItem button onClick={() => console.log("Instagram")}>
+          Instagram
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default SideDrawer;
